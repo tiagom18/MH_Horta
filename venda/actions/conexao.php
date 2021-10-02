c53a6ecc526428fff1eab95c26b933ea673d1038
Gabriel Stênio <gabri.stnio50@hotmail.com>
2021-10-02 10:02:38 -0300

    Tela venda

--- venda/actions/conexao.php
+++ venda/actions/conexao.php
@@ -1,7 +1,7 @@
 <?php
     try {
         $conexao = new PDO("mysql:host=localhost; dbname=crud_simples", "root", "");
-        $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE EXCEPTION);
+        $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $conexao->exec("set names utf8");
     } catch (PDOException $erro) {
         echo "Erro na conexão: ".$erro->getMessage();
