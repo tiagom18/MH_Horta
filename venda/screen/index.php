0b35818b5097ee5f9c9624efc07167ee04f53563
Gabriel Stênio <gabri.stnio50@hotmail.com>
2021-10-04 22:22:15 -0300

    modal (pop-up) feito e mais alguns elementos

--- venda/screen/index.php
+++ venda/screen/index.php
@@ -1,3 +1,8 @@
+<?php
+    session_start();
+    include('../actions/conexao.php')
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -64,10 +69,10 @@
                         </ul>
                     </div>
                     <div class="cont-btn">
-                        <button type="submit" class="btn btn-logout">
+                        <a href="../actions/logout.php"><button type="submit" class="btn btn-logout">
                             <img class="btn-img" src="https://www.imagemhost.com.br/images/2021/10/01/exit_to_app.png" alt="exit_to_app.png" border="0">
                             <p class="btn-text">Logout</p>
-                        </button>
+                        </button></a>
                     </div>
                 </div>
             </div>
@@ -84,6 +89,71 @@
                         <p class="usuario-text">Administrador</p>
                     </div>
                 </div>
+                <div class="cont-crud">
+                    <div class="cont-01">
+                        <span class="title-venda">Venda</span>
+                        <div class="cont-filter">
+                            <div class="filter">
+                                <img class="filter-img" src="https://i.ibb.co/6v6P822/filter.png" alt="filter" border="0">
+                            </div>
+                            <select class="filter-select"></select>
+                            <button class="btn-add"></button>
+                        </div>
+                    </div>
+                    <div class="cont-02">
+                        <table class="cont-table">
+                            <thead>
+                                <th>
+                                <label class="conectado">
+                                    <input type="checkbox" value="conectado">
+                                    <span class="checkmark"></span>
+                                </label>
+                                </th>
+                                <th class="txt-id">ID</th>
+                                <th class="txt">Nome</th>
+                                <th class="txt">E-mail</th>
+                                <th class="txt">Celular</th>
+                                <th class="txt-actions">Actions</th>
+                            </thead>
+                        </table>
+                    </div>
+                    <div class="cont-04"></div>
+                </div>
+            <div id="modal-update" class="cont-modal">
+                <div class="modal">
+                    <button id="exit" class="btn-exit">
+                            <img class="exit-img" src="https://i.postimg.cc/8zY4r6HD/close.png" alt="exit" border="0">
+                        </button>
+                    <div class="cont-info">
+                        <span class="txt-form">Alterar</span>
+                        <hr>
+                        <form>
+                            <div class="form-id">
+                                <label for="id">ID</label>
+                                <input id="id" class="input-id" name="id" type="number" value="">
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <script>
+                function openModal(modalID) {
+                    const modal = document.getElementById(modalID);
+                    if(modal) {
+                        modal.classList.add('mostrar');
+                        modal.addEventListener('click', (event) => {
+                            if(event.target.id == modalID || event.target.className == 'exit-img') {
+                                modal.classList.remove('mostrar');
+                            }
+                        })
+                    }
+                }
+
+                const add = document.querySelector('.btn-add');
+                add.addEventListener('click', function() {
+                    openModal('modal-update');
+                })
+            </script>
         </section>
     </div>
 </body>
