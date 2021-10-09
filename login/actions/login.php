e06102c8c1301cfa6e7bba6a42c6a30eb3a85a94
ana-sr <anacaroline.soaresramos@gmail.com>
2021-10-09 20:17:10 -0300

    link login

--- login/actions/login.php
+++ login/actions/login.php
@@ -5,7 +5,7 @@
         if($_POST['email'] == 'admin@horta.com.br' && $_POST['senha'] == '123456') {
             $_SESSION['email'] = $_POST['email'];
             $_SESSION['senha'] = $_POST['senha'];
-            header('Location: example.php');
+            header('Location: ../../venda/index.php');
         } else {
             header('Location: ../screen/index.php');
         }
